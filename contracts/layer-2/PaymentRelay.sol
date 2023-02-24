a07f0e9582887ce0d6f3c31532f763a103c5c5c7
jamado95 <joao.amado@unagi.ch>
2023-02-24 02:39:26 +0000

    two-step payment process for pessimistic refunds

--- contracts/layer-2/PaymentRelay.sol
+++ contracts/layer-2/PaymentRelay.sol
@@ -21,6 +21,7 @@ contract PaymentRelay is AccessControl {
     struct Payment {
         address token;
         uint256 amount;
+        bool exec;
     }
 
     // (keccak256 UID => payment) mapping of payments
@@ -40,13 +41,18 @@ contract PaymentRelay is AccessControl {
         return keccak256(abi.encodePacked(UID, from));
     }
 
+    function isPaymentReserved(bytes32 UID, address from) public view returns (bool) {
+        Payment memory payment = _getPayment(UID, from);
+        return payment.amount > 0 && !payment.exec;
+    }
+
     function isPaymentProcessed(bytes32 UID, address from)
         public
         view
         returns (bool)
     {
         Payment memory payment = _getPayment(UID, from);
-        return payment.amount > 0;
+        return payment.exec;
     }
 
     function getPayment(bytes32 UID, address from)
@@ -73,32 +79,48 @@ contract PaymentRelay is AccessControl {
         uint256 amount
     ) private {
         require(
-            !isPaymentProcessed(UID, from),
-            "PaymentRelay: Payment already processed"
+            !isPaymentReserved(UID, from) || !isPaymentProcessed(UID, from), 
+            "PaymentRelay: Payment already processed or reserved"
         );
 
-        _payments[getPaymentKey(UID, from)] = Payment(token, amount);
+        _payments[getPaymentKey(UID, from)] = Payment(token, amount, false);
     }
 
-    function execPayment(
+    function reservePayment(
         address tokenAddress,
         uint256 amount,
-        bytes32 UID,
-        address forwardTo
+        bytes32 UID
     ) external {
         _checkRole(TOKEN_ROLE, tokenAddress);
         require(
             amount > 0,
             "PaymentRelay: Payment amount should be strictly positive"
         );
-        _checkRole(RECEIVER_ROLE, forwardTo);
 
         _reservePayment(UID, msg.sender, tokenAddress, amount);
-        IERC20 tokenContract = IERC20(tokenAddress);
-        tokenContract.safeTransferFrom(msg.sender, forwardTo, amount);
 
-        emit PaymentSent(UID, msg.sender, tokenAddress, amount);
+        emit PaymentReserved(UID, msg.sender, tokenAddress, amount);
     }
 
+    function execPayment(
+        bytes32 UID,
+        address forwardTo
+    ) external {
+        require(
+            isPaymentReserved(UID, msg.sender),
+            "PaymentRelay: Payment reserve for message sender not found"
+        );
+        _checkRole(RECEIVER_ROLE, forwardTo);
+
+        Payment storage payment = _payments[getPaymentKey(UID, msg.sender)];
+        IERC20 tokenContract = IERC20(payment.token);
+        tokenContract.safeTransferFrom(msg.sender, forwardTo, payment.amount);
+
+        payment.exec = true;
+
+        emit PaymentSent(UID, msg.sender, payment.token, payment.amount);
+    }
+
+    event PaymentReserved(bytes32 UID, address from, address token, uint256 amount);
     event PaymentSent(bytes32 UID, address from, address token, uint256 amount);
 }
